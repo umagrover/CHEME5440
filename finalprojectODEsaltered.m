b8be3b6224c95f705f7863580e33d08416ed3b47
umagrover <uag4@cornell.edu>
2026-05-12 14:02:13 -0700

    Update finalprojectODEsaltered.m

--- finalprojectODEsaltered.m
+++ finalprojectODEsaltered.m
@@ -1,4 +1,4 @@
-function dxdt = finalprojectODEsaltered(t,x,k,Aeff,pcd4,pcd70);
+function dxdt = finalprojectODEsaltered(t,x,k,Aeff);
 
 dxdt = zeros(6,1);
 
@@ -6,16 +6,16 @@ dxdt = zeros(6,1);
 dxdt(1) = -k(1)*x(1)*x(2) +k(2)*x(4) -k(3)*x(1)*x(3) +k(4)*x(5); 
 
 % free CD4 receptor 
-dxdt(2) = -k(1)*x(1)*x(2) +k(2)*x(4) -Aeff*pcd4*k(1)*x(5) +k(2)*x(6);  
+dxdt(2) = -k(1)*x(1)*x(2) +k(2)*x(4) -Aeff*k(1)*x(5) +k(2)*x(6);  
 
 % free CD70 receptor
-dxdt(3) = -k(3)*x(1)*x(3) +k(4)*x(5) -Aeff*pcd70*k(3)*x(4) +k(4)*x(6);
+dxdt(3) = -k(3)*x(1)*x(3) +k(4)*x(5) -Aeff*k(3)*x(4) +k(4)*x(6);
 
 % Ab:CD4a
-dxdt(4) = k(1)*x(1)*x(2) -k(2)*x(4) -Aeff*pcd70*k(3)*x(4) +k(4)*x(6);
+dxdt(4) = k(1)*x(1)*x(2) -k(2)*x(4) -Aeff*k(3)*x(4) +k(4)*x(6);
 
 % Ab:CD70
-dxdt(5) = k(3)*x(1)*x(3) -k(4)*x(5) -Aeff*pcd4*k(1)*x(5) +k(2)*x(6);
+dxdt(5) = k(3)*x(1)*x(3) -k(4)*x(5) -Aeff*k(1)*x(5) +k(2)*x(6);
 
 % CD4:Ab:CD70/second binding event
-dxdt(6) = Aeff*pcd4*k(1)*x(5) -k(2)*x(6) +Aeff*pcd70*k(3)*x(4) -k(4)*x(6);  
+dxdt(6) = Aeff*k(1)*x(5) -k(2)*x(6) +Aeff*k(3)*x(4) -k(4)*x(6);  
