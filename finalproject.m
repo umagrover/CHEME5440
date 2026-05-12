f333ec184948c59c6c528ae1a2f46f84512f3a81
umagrover <uag4@cornell.edu>
2026-05-11 23:09:12 -0700

    Update finalproject.m

--- finalproject.m
+++ finalproject.m
@@ -3,9 +3,10 @@
 cd('C:\Users\umaag\OneDrive\Cornell\CHEME 5440\')
 
 clear all; close all;
-%%
-% In nM
-x0(1) = 16; % initial concentration of free Ab (nM)
+%% 
+
+% Using Units of nM 
+x0(1) = 10; % initial concentration of free Ab (nM)
 x0(2) = 7.5 * 10^-2; % initial conc of free CD4 receptor (nM)
 x0(3) = 7.5 * 10^-2; % initial conc of free CD70 receptor (nM)
 x0(4) = 0; % initial concentration of Ab:CD4 (nM)
@@ -23,176 +24,53 @@ kappa = 2*10^6; % avidity parameter, from textbook
 tspan = [0 60*60]; % time-span in sec
 
 [t_out,x_out] = ode15s(@(t,x) finalprojectODEs(t,x,k,kappa),tspan,x0);
+% Kd CD70 = around 70 nM
 
-%Redfining CD70
-k(1) = 5.2 * 10^-4; % kon, CD4 (nM^-1 s^-1)
-k(2) = 3.6 * 10^-2; % koff, CD4 (s^-1)
-% Kd CD70 = around 70 nM 
-
-[t_out1,x_out1] = ode15s(@(t,x) finalprojectODEs(t,x,k,kappa),tspan,x0);
-
-figure(1); 
-subplot(2,3,1);
-plot(t_out./60,x_out(:,1),'-r');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('antibody');
-grid on;
-
-subplot(2,3,2);
-plot(t_out./60,x_out(:,2),'-r');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('cd4 receptor');
-grid on;
-
-subplot(2,3,3);
-plot(t_out./60,x_out(:,3),'-r');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('cd70');
-grid on;
-
-subplot(2,3,4);
-plot(t_out./60,x_out(:,4),'-r');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('Ab:CD4');
-grid on;
-
-subplot(2,3,5);
-plot(t_out./60,x_out(:,5),'-r');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('Ab:CD70');
-grid on;
-
-subplot(2,3,6);
-plot(t_out./60,x_out(:,6),'-r');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('CD4:Ab:CD70');
-grid on;
-
-figure(2); 
-subplot(2,3,1);
-plot(t_out./60,x_out(:,1),'-bo');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('antibody');
-grid on;
-
-subplot(2,3,2);
-plot(t_out./60,x_out(:,2),'-bo');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('cd4 receptor');
-grid on;
-
-subplot(2,3,3);
-plot(t_out./60,x_out(:,3),'-bo');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('cd70');
-grid on;
-
-subplot(2,3,4);
-plot(t_out./60,x_out(:,4),'-bo');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('Ab:CD4');
-grid on;
-
-subplot(2,3,5);
-plot(t_out./60,x_out(:,5),'-bo');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('Ab:CD70');
-grid on;
-
-subplot(2,3,6);
-plot(t_out./60,x_out(:,6),'-bo');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-title('CD4:Ab:CD70');
-grid on;
-
-figure(3);
-hold on;
-plot(t_out./60,x_out(:,4),'-bo');
-plot(t_out./60,x_out(:,5),'-go');
-plot(t_out./60,x_out(:,6),'-ro');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-legend('Ab:CD4','Ab:CD70','CD4:Ab:CD70');
-grid on;
-hold off; 
-
-figure(4);
-for i = 1:6
-    subplot(2,3,i);
-    plot(t_out1./60,x_out1(:,i),'-r');
-    xlabel('Time (min)');
-    ylabel('Concentration (nM)');
-    % title('CD4:Ab:CD70');
-    grid on;
-end
-
-figure(5);
-hold on;
-plot(t_out1./60,x_out1(:,4),'-bo');
-plot(t_out1./60,x_out1(:,5),'-go');
-plot(t_out1./60,x_out1(:,6),'-ro');
-xlabel('Time (min)');
-ylabel('Concentration (nM)');
-legend('Ab:CD4','Ab:CD70','CD4:Ab:CD70');
-title(" now its more specific hopefully");
-grid on;
-hold off; 
-
-%% Recreation of Mazor et al. figure
-% Constants
+%% Recreating the Mazor et al. Figure
+% Global Constants
 tspan = [0 60*60];        % Time for equilibrium
-conv_factor = 9e8;      % Scale to #/cell
+conv_factor = 9e8; %300000/0.001; %cells/liter
 Ab_range = logspace(-11, -7, 25); 
 kd_scenarios = [0.9, 70]; 
 results = cell(1, 2);
-avagardo = 6.022e23;
+avagadro = 6.022e23;
+kappa= 2e6;
 
-% Simulation to calculate values at each concentration of Ab
 for s = 1:2
     % Set kinetic rates
-    k(1) = 2.8 * 10^-4;              % kon, CD4
+    k(1) = 2.8 * 10^-4;
+    %k(1) = 2.8 * 10^-4;              % kon, CD4
     k(2) = k(1) * kd_scenarios(s);   % koff, CD4 
     k(3) = 2.0 * 10^-4;              % kon, CD70
     k(4) = k(3) * 25;                % koff, CD70 
-    kappa = 3*10^6;                  
-    
+    %kappa = (kd_scenarios(s)/25)*avagadro/conv_factor; %using the derivaition from the textbook
+
     bound_data = zeros(length(Ab_range), 3); 
 
     for i = 1:length(Ab_range)
         Ab_init_nM = Ab_range(i) * 1e9;
         
         % CD4+/CD70+ 
-        x0 = [Ab_init_nM, 0.075, 0.075, 0, 0, 0];
+        x0 = [Ab_init_nM, (4.6e4/avagadro)*conv_factor*1e9, (5.2e4/avagadro)*conv_factor*1e9, 0, 0, 0];
+        %x0 = [Ab_init_nM, 0.075, 0.075, 0, 0, 0];
         [~, x_out] = ode15s(@(t,x) finalprojectODEs(t,x,k,kappa), tspan, x0);
-        bound_data(i, 1) = (sum(x_out(end, 4:6)) * avagardo * 1e-9)/conv_factor;
+        bound_data(i, 1) = (sum(x_out(end, 4:6)) * avagadro * 1e-9)/conv_factor;
         
         % CD4+/CD70-
-        x0 = [Ab_init_nM, 0.075, 0, 0, 0, 0];
+        x0 = [Ab_init_nM, (3.8e4/avagadro)*conv_factor*1e9, 0, 0, 0, 0];
         [~, x_out] = ode15s(@(t,x) finalprojectODEs(t,x,k,kappa), tspan, x0);
-        bound_data(i, 2) = (sum(x_out(end, 4:6)) * avagardo * 1e-9)/conv_factor;
+        bound_data(i, 2) = (sum(x_out(end, 4:6)) * avagadro * 1e-9)/conv_factor;
         
         % CD4-/CD70+
-        x0 = [Ab_init_nM, 0, 0.075, 0, 0, 0];
+        x0 = [Ab_init_nM, 0, (3.1e4/avagadro)*conv_factor*1e9, 0, 0, 0];
         [~, x_out] = ode15s(@(t,x) finalprojectODEs(t,x,k,kappa), tspan, x0);
-        bound_data(i, 3) = (sum(x_out(end, 4:6)) * avagardo * 1e-9)/conv_factor;
+        bound_data(i, 3) = (sum(x_out(end, 4:6)) * avagadro * 1e-9)/conv_factor;
     end
     results{s} = bound_data;
 end
 
-% Creating the Plot
-figure(6); % Explicitly naming this Figure 6
+% Plotting
+figure(6); 
 set(gcf, 'Position', [100, 100, 1000, 400]); % Set window size
 
 for s = 1:2
@@ -212,8 +90,6 @@ for s = 1:2
     ylim([0 6e4]);
     grid on;
     
-    % Labels located at end of curves
-    text(2e-8, data(end,1), 'CD4^+/CD70^+', 'FontSize', 8);
-    text(2e-8, data(end,2), 'CD4^+/CD70^-', 'FontSize', 8);
-    text(2e-8, data(end,3), 'CD4^-/CD70^+', 'FontSize', 8);
+    legend('CD4+/CD70+', 'CD4+/CD70-','CD4-/CD70+')
+
 end
